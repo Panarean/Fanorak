2c67f09654d74a3c0c0c711f638b18b5f0c84755
Panarean <PanareanMasproton.me@proton.me>
2023-09-28 11:47:17 -0700

    111

--- fanorak/src/config/index.js
+++ fanorak/src/config/index.js
@@ -1,2 +1,2 @@
-export const BackendURL='https://localhost:3000'
-export const Current = 'https://localhost:5173'
+export const BackendURL='https://fotom.finance:3000'
+export const Current = 'https://fotom.finance:5173'
